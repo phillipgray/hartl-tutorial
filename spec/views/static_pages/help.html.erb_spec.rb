1398dacd3042e8c4bef86306f6e19b565b2e9415
Phillip Gray <phillipgray@Phillips-MacBook-Air.local>
2015-11-07 20:57:05 -0500

    Finishes static pages

--- spec/views/static_pages/help.html.erb_spec.rb
+++ spec/views/static_pages/help.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/help.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders the correct title" do
+    render template: "static_pages/help", layout: "layouts/application"
+    expect(rendered).to have_title("Help | Ruby on Rails Tutorial Sample App")
+  end
 end
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders the correct title" do
+    render template: "static_pages/home", layout: "layouts/application"
+    expect(rendered).to have_title('Home | Ruby on Rails Tutorial Sample App')
+  end
 end
